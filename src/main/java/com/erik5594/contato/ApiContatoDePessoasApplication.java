95924bdb6c5bc54b37bac6e71db6b3ac07081bbb
erik-qo <erik.derick74@gmail.com>
2021-07-21 19:36:58 -0300

    Criação do projeto - API REST Cadastro de pessoas e contatos

---
+++ src/main/java/com/erik5594/contato/ApiContatoDePessoasApplication.java
@@ -0,0 +1,13 @@
+package com.erik5594.contato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiContatoDePessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiContatoDePessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erik5594/contato/ApiContatoDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erik5594.contato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiContatoDePessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
